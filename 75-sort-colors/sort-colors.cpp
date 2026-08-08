44bb608ff6e2d00ddb438a122c9e3312c7b72305
LALIT GURJAR <gujarlalit79@gmail.com>
2026-08-08 22:26:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (12.04%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+int partition(vector<int>& arr, int low, int high) {
+  
+    // choose the pivot
+    int pivot = arr[high];
+  
+    // undex of smaller element and indicates 
+    // the right position of pivot found so far
+    int i = low - 1;
+
+    // Traverse arr[low..high] and move all smaller
+    // elements on left side. Elements from low to 
+    // i are smaller after every iteration
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    
+    // move pivot after smaller elements and
+    // return its position
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+
+// the QuickSort function implementation
+void quickSort(vector<int>& arr, int low, int high) {
+  
+    if (low < high) {
+      
+        // pi is the partition return index of pivot
+        int pi = partition(arr, low, high);
+
+        // recursion calls for smaller elements
+        // and greater or equals elements
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+  
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+         quickSort(nums, 0, n - 1);
+      
+    }
+};
